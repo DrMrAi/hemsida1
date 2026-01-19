17b03339285bbdd72cb521700ff8096c59dd0c9b
ludvigPublik <ludvigpublik@gmail.com>
2026-01-19 21:51:46 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from 'public' folder
+app.use(express.static('public'));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to my Node.js website!</h1>');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+1. Download node.js from https://nodejs.org/en/download
+2. Run these commands:
+cd hemsida1
+pnpm install
+pnpm node app.js
+5. Open http://localhost:3000 in webbrowser
+
+
+Notes
+Hämta data från https://tcgcsv.com/
+
