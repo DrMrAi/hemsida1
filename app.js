b02bd16e9a6ac5e73abb1bcc40123a205121e74e
ludvigPublik <ludvigpublik@gmail.com>
2026-01-27 17:16:58 +0100

    la till product och grund för api

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // app.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from 'public' folder
+// Serve static files (CSS, images, JS)
 app.use(express.static('public'));
 
-// Basic route
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to my Node.js website!</h1>');
+// Dynamic product route
+app.get('/product/:id', (req, res) => {
+  const productId = req.params.id;   // 88833 from URL
+  console.log("Requested product:", productId);
+
+  // Send product page HTML
+  res.sendFile(path.join(__dirname, 'public', 'product.html'));
 });
 
-app.get('/productexample', (req, res) => {
-  res.send('<h1>Welcome to my Node.js website!</h1>');
+// Test route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 // Start server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
